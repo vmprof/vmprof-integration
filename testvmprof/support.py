b1bb758595e3f16a6625e727f2456b9368771a8f
Richard Plangger <planrichi@gmail.com>
2016-07-21 16:19:21 +0200

    for now download the new-jit-log branch (should change later)

--- testvmprof/support.py
+++ testvmprof/support.py
@@ -6,7 +6,7 @@ import os
 
 class BaseVMProfTest(object):
     def setup_class(self):
-        tmp, pypy = setup_local_pypy(version='latest')
+        tmp, pypy = setup_local_pypy(branch='new-jit-log', version='latest')
         self.tmp = tmp
         self.pypy = os.path.join(tmp, pypy)
         self.vmprofargs = "--web --web-url %s" % self.vmprof_url
@@ -38,12 +38,12 @@ class BaseVMProfTest(object):
             "err\n%s\n\nout\n%s\n" % (err.decode(), outs.decode())
         return outs, err, proc.returncode
 
-def setup_local_pypy(version='latest', dist='linux64'):
+def setup_local_pypy(branch='default', version='latest', dist='linux64'):
     # uff, hardcoded paths & only for linux
     filename = "pypy.tar.bz2"
     tmp = tempfile.mkdtemp()
     if "TEST_PYPY_EXEC" not in os.environ:
-        download_pypy(os.path.join(tmp, filename), version, dist)
+        download_pypy(branch, os.path.join(tmp, filename), version, dist)
         subprocess.run(["tar", "xf", os.path.join(tmp, filename)], cwd=tmp)
         executable = None
         found = False
@@ -55,16 +55,16 @@ def setup_local_pypy(version='latest', dist='linux64'):
                     break
         if not found:
             raise AssertionError("could not setup local pypy")
+        absexe = os.path.join(tmp, executable)
     else:
-        executable = os.environ["TEST_PYPY_EXEC"]
-    absexe = os.path.join(tmp, executable)
+        absexe = executable = os.environ["TEST_PYPY_EXEC"]
     # poor man's dependency resolution
     subprocess.run(["virtualenv", "-p", absexe, "pypy-env"], cwd=tmp)
     subprocess.run(["pypy-env/bin/pip", "install", "--pre", "vmprof"], cwd=tmp)
     return tmp, os.path.join("pypy-env","bin","python")
 
-def download_pypy(path, version='latest', dist='linux64'):
-    link = "http://buildbot.pypy.org/nightly/default/pypy-c-jit-{}-{}.tar.bz2".format(version, dist)
+def download_pypy(path, branch, version, dist):
+    link = "http://buildbot.pypy.org/nightly/{}/pypy-c-jit-{}-{}.tar.bz2".format(branch, version, dist)
     r = requests.get(link, stream=True)
     if r.status_code == 200:
         with open(path, 'wb') as f:
--- testvmprof/test/examples/boot_env.py
+++ testvmprof/test/examples/boot_env.py
@@ -9,5 +9,4 @@ activate_this_file = os.path.join(path, "pypy-env/bin/activate_this.py")
 execfile(activate_this_file, dict(__file__=activate_this_file))
 
 sys.argv = sys.argv[2:] # -- will be stripped off by vmprof
-print(sys.argv)
 runpy.run_module('vmprof', run_name='__main__')
